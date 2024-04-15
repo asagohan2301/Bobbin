ebfd403792c666024bd1d199cd5c1bd52ac853a3
asagohan2301 <shemomechama@yahoo.co.jp>
2024-04-15 18:19:23 +0900

    corsの設定を修正

--- bobbin_api/config/initializers/cors.rb
+++ bobbin_api/config/initializers/cors.rb
@@ -1,8 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
-    resource "/api/products/*",
+    cors_origins = ENV.fetch("CORS_ALLOWED_ORIGINS", "localhost:3000").split(',')
+    origins cors_origins
+
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete]
+      methods: [:get, :post, :put, :patch, :delete],
+      credentials: false
   end
 end
