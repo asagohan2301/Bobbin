eab31d761cce5283555bc8948224de3e351618e8
asagohan2301 <shemomechama@yahoo.co.jp>
2024-05-14 21:38:52 +0900

    ログインユーザーのgroup_idとuser_idをコンテキストに設定

--- bobbin_frontend/src/app/login/page.tsx
+++ bobbin_frontend/src/app/login/page.tsx
@@ -2,9 +2,10 @@
 
 import ButtonWithIcon from '@/components/ButtonWithIcon'
 import Input from '@/components/Input'
+import { LoginUserContext } from '@/contexts/LoginUserContext'
 import type { ErrorsApiResponse } from '@/types/errorTypes'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Check, X } from 'react-bootstrap-icons'
 import { setCookie } from '../../utils/cookieUtils'
 
@@ -13,6 +14,13 @@ export default function Login() {
   const [password, setPassword] = useState<string>('')
 
   const router = useRouter()
+
+  const context = useContext(LoginUserContext)
+  if (!context) {
+    console.error('コンテキストの取得に失敗しました')
+    return
+  }
+  const { setLoginUserGroupId, setLoginUserId } = context
 
   const handleLogin = async () => {
     const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT
@@ -34,6 +42,8 @@ export default function Login() {
       await setCookie('firstName', user.first_name)
       await setCookie('lastName', user.last_name)
       await setCookie('token', user.token)
+      setLoginUserGroupId(user.group_id)
+      setLoginUserId(user.user_id)
       router.push('/')
     } else {
       const data: ErrorsApiResponse = await res.json()
